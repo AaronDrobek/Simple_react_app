ac6b3ee14ba240b9ba3f715883be45253d9e5f80
Aaron Drobek <adrobek09@gmail.com>
2017-09-14 16:21:55 -0400

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,7 @@
 import React, { Component } from 'react';
+import EarthquakeInfo from './EarthquakeInfo.js'
+import EarthquakeList from './EarthquakeList.js'
 import '../styles/App.css';
-import earthquakes from '../data/actors.js';
-
-class EarthquakeList extends Component{
-  render(){
-
-    let earthqaukesArray = earthquakes.map(earthquake => {
-      return (
-        <div className="col-sm-6" key={earthquake.id}>
-          <div className="card" >
-            <div className="card-block">
-              <h4 className="card-title">{earthquake.place}</h4>
-              <h6 className="card-subtitle mb-2 text-muted">Magnitude: {earthquake.mag}</h6>
-              <h6 className="card-subtitle mb-2 text-muted">Time: {moment({earthquake.time}).format('llll')}</h6>
-              <p className="card-text">Coordinates: {earthquake.coordinates}</p>
-
-              <a href={earthquake.url} className="card-link">USGS Event Link</a>
-
-            </div>
-          </div>
-        </div>
-      )
-    });
-
-    return (
-    <div className="quake-list">
-      <div className="row">
-            {quakes}
-      </div>
-    </div>
-  )
-}
-}
-
-
-
-
-
-
-
-
-
 
 
 class App extends Component {
@@ -48,13 +9,15 @@ class App extends Component {
     return (
       <div className="App">
         <div className="title">
+
+
           <div className="my-header">
           "Earthquakes!"
           </div>
+          <EarthquakeInfo />
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <EarthquakeList />
       </div>
     );
   }
